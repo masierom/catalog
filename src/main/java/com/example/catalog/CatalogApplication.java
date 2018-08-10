76ff9ca3de5d453ad5fd4ae0a3ea384df80742a8
Mirko Masiero <mirko.masiero@marconirovereto.it>
2018-08-10 10:57:44 +0200

    Uncomment spring load balancer

--- src/main/java/com/example/catalog/CatalogApplication.java
+++ src/main/java/com/example/catalog/CatalogApplication.java
@@ -2,16 +2,16 @@ package com.example.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;*/
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CatalogApplication {
 	
-	/*@LoadBalanced @Bean RestTemplate restTemplate() {
+	@LoadBalanced @Bean RestTemplate restTemplate() {
 		return new RestTemplate();
-	}*/
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
